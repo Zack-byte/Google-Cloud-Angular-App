90f555b229ac3b47c746d5b2d78927bfce797250
Zachary Harris <zacharyharris27@gmail.com>
2022-09-23 08:19:03 -0400

    # having some Fun

--- cloud-app/src/app/app.component.ts
+++ cloud-app/src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cloud-app';
   webApiUrl = environment.webapiurl;
+
+  public ngOnInit(): void {
+    document.addEventListener('mousemove', (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      const anchor = document.getElementById('anchor');
+      const rekt = anchor?.getBoundingClientRect();
+      if (rekt != undefined) {
+        const anchorX = rekt.left + rekt.width / 2;
+        const anchorY = rekt.top + rekt.height / 2;
+
+        const angleDeg = this.angle(mouseX, mouseY, anchorX, anchorY);
+
+        const eyes = document.querySelectorAll('.eye');
+
+        if (eyes != null) {
+          eyes.forEach((eye: any) => {
+            eye.style.transform = `rotate(${angleDeg + 90}deg)`;
+          });
+        }
+      }
+    });
+  }
+
+  public angle(cx: number, cy: number, ex: number, ey: number): number {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = (rad * 180) / Math.PI;
+    return deg;
+  }
 }
